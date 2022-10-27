41cd26ebde1aae2ab919bdad602adefef363964d
marian <golgisistem@gmail.com>
2022-10-27 23:51:54 +0300

    posentegra

--- setup.bat
+++
@@ -1,9 +0,0 @@
-cd "C:/"
-call git clone https://github.com/puuble/posentegra.git peClient
-cd peClient
-call npm install
-call npm install pm2 -g
-call npm install pm2-windows-startup -g
-call pm2 start bin\www -n "PosEntegra"
-call pm2 save
-
--- update.bat
+++
@@ -1,7 +0,0 @@
-cd "C:/peClient"
-
-call git pull origin main
-call npm install
-call pm2 restart all
-
-
