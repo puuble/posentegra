ba6e1e6a5fb84ee410c964a034457a85152dbb2e
marian <golgisistem@gmail.com>
2022-10-29 00:02:21 +0300

    setup

--- update.bat
+++ update.bat
@@ -2,7 +2,6 @@ cd "C:/peClient"
 
 call git fetch origin posentegra
 call git reset --hard FETCH_HEAD
-call git config --global --add safe.directory C:/peClient
 call git clean -df
 call npm install
 call pm2 restart all
