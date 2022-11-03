101b9ac6c57aa67384d4fd0c45e1bf6ac9125a26
marian <golgisistem@gmail.com>
2022-11-03 11:17:33 +0300

    authcheck

--- bin/start.js
+++ bin/start.js
@@ -1,10 +1,12 @@
 const { getEnvironment } = require('./lib/helpers')
 const PusherClient = require('./lib/pusher')
+const Sambapos = require('./lib/sambapos')
 const pusher = new PusherClient()
-
+const sambapos = new Sambapos()
 async function signin() {
   let env = getEnvironment()
   if (env) {
+    await sambapos.authCheck()
     await pusher.connect()
   }
 }
