875fb9a206b4993a2eb1b30c394c58be0b8dd16e
marian <golgisistem@gmail.com>
2022-11-04 22:57:45 +0300

    test

--- sambaposTest.js
+++ sambaposTest.js
@@ -4,5 +4,7 @@ const sambapos = new Sambapos()
 async function main() {
   let s = await sambapos.authCheck()
   console.log(s)
+  let menu = await sambapos.getProducts()
+  console.log(menu)
 }
 main()
