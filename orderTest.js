e012cb7f88a047b69dc1e7c67d4ee4eeb17fc41b
marian <golgisistem@gmail.com>
2022-11-05 20:49:34 +0300

    test

--- orderTest.js
+++ orderTest.js
@@ -1,26 +1,34 @@
 let queries = {
   queries: {
     addEntity:
-      'mutation m{addEntity(entity:{entityType:"Müşteriler",name:"Çağla D - 544 977 57 87"}){name}}',
+      'mutation m{addEntity(entity:{entityType:"Müşteriler",name:"tanya j - 530 567 45 74"}){name}}',
     updateEntityCustomData: [
-      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Çağla D - 544 977 57 87", name: "Adres", value: "Malazgirt 1071 Cad. no:göksu relax sit. daire no:228 b blok kat:7-[ Apt. No: null][ Kat : null][ Kapı No: null] - Etimesgut (Şht Osman Avcı Mah.)[ - Açıklama: 228 | Goksu relax B blok kat:7 daire no:228; - ** ÇATAL-BIÇAK GÖNDERMEYİN]"){name}\n}',
-      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Çağla D - 544 977 57 87", name: "Telefon", value: "544 977 57 87"){name}\n}',
-      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Çağla D - 544 977 57 87", name: "Bölge", value: "Etimesgut (Şht Osman Avcı Mah.)"){name}\n}',
-      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Çağla D - 544 977 57 87", name: "Ad Soyad", value: "Çağla D"){name}\n}',
+      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "tanya j - 530 567 45 74", name: "Adres", value: "Sevinç Sk. no: 72 daire no:2 Güvendik 2 Hasan Yaşar apt. kat:1-[ Apt. No: null][ Kat : null][ Kapı No: null] - Kuşadası (Türkmen Mah.)[ - Açıklama: 2 | köşedeki bina;]"){name}\n}',
+      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "tanya j - 530 567 45 74", name: "Telefon", value: "530 567 45 74"){name}\n}',
+      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "tanya j - 530 567 45 74", name: "Bölge", value: "Kuşadası (Türkmen Mah.)"){name}\n}',
+      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "tanya j - 530 567 45 74", name: "Ad Soyad", value: "tanya j"){name}\n}',
     ],
     registerTerminal:
       'mutation m {registerTerminal(user: "Yemek Sepeti", ticketType: "Paket Servis", terminal: "Sunucu", department: "Restoran")}',
     createTerminalTicket:
       'mutation m {createTerminalTicket(terminalId:"{terminalId}"){uid}}',
     updateTerminalTicket:
-      'mutation m {  updateTerminalTicket(terminalId: "{terminalId}", note: "Servis plastik çatal, peçete vs İSTEMİYORUM | Temassız Teslimat İstiyorum", states: [{"stateName":"Kaynak","state":"Yemek Sepeti"},{"stateName":"Ödeme Şekli","state":""},{"stateName":"Teslimat Şekli","state":"2"}], tags: [{"tagName":"Yemek Sepeti Onay Kodu","tag":"68879992"},{"tagName":"Ödeme Şekli","tag":"Online Kredi\\/Banka Kartı - Sipariş tutarı internet üzerinden ÖDENMİŞTİR. - Lütfen fiş getiriniz."},{"tagName":"ID","tag":""},{"tagName":"Restoran","tag":"Nokta Pide Test"}]){id}}',
+      'mutation m {  updateTerminalTicket(terminalId: "{terminalId}", note: "Temassız Teslimat İstiyorum | Servis plastik çatal, peçete vs İSTEMİYORUM", states: [{"stateName":"Kaynak","state":"Yemek Sepeti"},{"stateName":"Ödeme Şekli","state":""},{"stateName":"Teslimat Şekli","state":"2"}], tags: [{"tagName":"Yemek Sepeti Onay Kodu","tag":"68264515"},{"tagName":"Ödeme Şekli","tag":"Online Kredi\\/Banka Kartı - Sipariş tutarı internet üzerinden ÖDENMİŞTİR. - Lütfen fiş getiriniz."},{"tagName":"ID","tag":""},{"tagName":"Restoran","tag":"Meşhur Yenipazar Pidecisi Test 2"}]){id}}',
     addOrderToTerminalTicket: [
-      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:6147,price:85.00,quantity:1,portion:"Normal",orderTags:"Yemeksepeti Etiketi=2 x Lahmacun:Acısız,Yemeksepeti Etiketi=Lahmacun:Acılı"){orders{uid}}}',
+      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:5626,price:27.00,quantity:1,portion:"Normal",orderTags:""){orders{uid}}}',
+    ],
+    addProduct: [
+      'mutation m {\n                addProduct(name: "Kıymalı Yumurtalı Pide - 1 Porsiyon", groupCode: "Entegrasyon", portions: {name: "Normal", price:1}) {\n                  id\n                }\n              }',
+      'mutation m {\n                addProduct(name: "Bozdoğan Peynirlisi - 1 Porsiyon", groupCode: "Entegrasyon", portions: {name: "Normal", price:1}) {\n                  id\n                }\n              }',
+    ],
+    getProduct: [
+      { name: 'Kıymalı Yumurtalı Pide - 1 Porsiyon' },
+      { name: 'Bozdoğan Peynirlisi - 1 Porsiyon' },
     ],
     addCalculationToTerminalTicket:
       'mutation m { addCalculationToTerminalTicket(terminalId:"{terminalId}",calculationName:"Joker İskonto",amount:0.00){id}}',
     changeEntityOfTerminalTicket:
-      'mutation m {changeEntityOfTerminalTicket (terminalId:"{terminalId}",type:"Müşteriler",name:"Çağla D - 544 977 57 87"){uid}}',
+      'mutation m {changeEntityOfTerminalTicket (terminalId:"{terminalId}",type:"Müşteriler",name:"tanya j - 530 567 45 74"){uid}}',
     closeTerminalTicket:
       'mutation m {closeTerminalTicket(terminalId:"{terminalId}")}',
     getTerminalTickets:
@@ -33,10 +41,10 @@ let queries = {
       'mutation m { postBroadcastMessage(message: "ENT-YeniSiparis-ys") {\n                              message\n                            }\n                          }',
   },
   order: {
-    id: '1554203817',
-    pid: '1554203817',
+    id: '1554057901',
+    pid: '1554057901',
     slug: 'ys',
-    restaurantId: '63668881164d597e970a910e',
+    restaurantId: '63617059d55d1a6201061679',
   },
 }
 const Query = require('./bin/lib/query')
