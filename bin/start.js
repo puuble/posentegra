344dbbd30fba06ff19067bdef8e189f24faa510b
marian <golgisistem@gmail.com>
2022-11-16 13:54:09 +0200

    test

--- bin/start.js
+++ bin/start.js
@@ -1,12 +1,16 @@
 const { getEnvironment } = require('./lib/helpers')
 const PusherClient = require('./lib/pusher')
+const Query = require('./lib/query')
 const Sambapos = require('./lib/sambapos')
 const pusher = new PusherClient()
 const sambapos = new Sambapos()
+const query = new Query()
+
 async function signin() {
   let env = getEnvironment()
   if (env) {
-    console.log('sambapos check ')
+    let products = await query.getProducts()
+    console.log(products, 'sambapos check ')
     await sambapos.authCheck()
     await pusher.connect()
   }
