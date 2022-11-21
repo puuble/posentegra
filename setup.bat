6caa493fd9ae5e6fcee4555ac7a3e0a5a5655e9b
marian <golgisistem@gmail.com>
2022-11-21 21:52:49 +0200

    test

--- setup.bat
+++ setup.bat
@@ -1,12 +1,11 @@
-cd "C:/"
+cd C:/
 call git clone --branch posentegra --single-branch https://github.com/puuble/posentegra.git peClient
 cd peClient
 call git config --global --add safe.directory C:/peClient
 call npm install
 call npm install pm2 -g
 call npm install pm2-windows-startup -g
-call npm install pm2-startup -g
+call pm2-startup install -g
 call pm2 start bin\www -n "PosEntegra"
 call pm2 save
 call node ./bin/setup.js
-
