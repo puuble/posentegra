3e0eaacbba53de30d6e900df191188621534fed1
marian <golgisistem@gmail.com>
2022-11-25 23:03:21 +0200

    test

--- orderTest.js
+++ orderTest.js
@@ -1,44 +1,36 @@
 let queries = {
   queries: {
     addEntity:
-      'mutation m{addEntity(entity:{entityType:"Müşteriler",name:"Gizem h - 542 772 53 19"}){name}}',
+      'mutation m{addEntity(entity:{entityType:"Müşteriler",name:"Furkan N - 5302790647"}){name}}',
     updateEntityCustomData: [
-      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Gizem h - 542 772 53 19", name: "Adres", value: "610. Sk. no:1 daire no:65 Tnr garden loft kat:5-[ Apt. No: null][ Kat : null][ Kapı No: null] - İkiçeşmelik Kuşadası[ - Açıklama: 65 | TNR garden loft kat:5 daire:5;]"){name}\n}',
-      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Gizem h - 542 772 53 19", name: "Telefon", value: "542 772 53 19"){name}\n}',
-      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Gizem h - 542 772 53 19", name: "Bölge", value: "İkiçeşmelik Kuşadası"){name}\n}',
-      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Gizem h - 542 772 53 19", name: "Ad Soyad", value: "Gizem h"){name}\n}',
+      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Furkan N - 5302790647", name: "Adres", value: "5 Sk. no: 158-[ Apt. No: null][ Kat : null][ Kapı No: null] - Artuklu (Nur Mah.)[ - Açıklama: Nur mahallesi 24.sokak Devlet hastanesi arkası konbay sitesi altı number one fitness;]"){name}\n}',
+      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Furkan N - 5302790647", name: "Telefon", value: "5302790647"){name}\n}',
+      'mutation m {\n  updateEntityCustomData(entityTypeName: "Müşteriler", entityName: "Furkan N - 5302790647", name: "Ad Soyad", value: "Furkan N"){name}\n}',
     ],
     registerTerminal:
       'mutation m {registerTerminal(user: "Yemek Sepeti", ticketType: "Paket Servis", terminal: "Sunucu", department: "Restoran")}',
     createTerminalTicket:
       'mutation m {createTerminalTicket(terminalId:"{terminalId}"){uid}}',
     updateTerminalTicket:
-      'mutation m {  updateTerminalTicket(terminalId: "{terminalId}", note: "Servis plastik çatal, peçete vs İSTEMİYORUM", states: [{"stateName":"Kaynak","state":"Yemek Sepeti"},{"stateName":"Ödeme Şekli","state":""},{"stateName":"Teslimat Şekli","state":"2"}], tags: [{"tagName":"Yemek Sepeti Onay Kodu","tag":"70142347"},{"tagName":"Ödeme Şekli","tag":"Kredi Kartı (Sipariş tesliminde kredi kartı \\/ banka kartı ile ödeme) - Lütfen fiş getiriniz."},{"tagName":"ID","tag":""},{"tagName":"Restoran","tag":"Meşhur Yenipazar Pidecisi Test 2"}]){id}}',
+      'mutation m {  updateTerminalTicket(terminalId: "{terminalId}", note: "Servis plastik çatal, peçete vs İSTEMİYORUM[ - Teslimat Saati: null]", states: [{"stateName":"Kaynak","state":"Yemek Sepeti"},{"stateName":"Ödeme Şekli","state":"Kredi Kartı"}], tags: [{"tagName":"Ödeme Şekli","tag":"Kredi Kartı (Sipariş tesliminde kredi kartı \\/ banka kartı ile ödeme) - Lütfen fiş getiriniz."},{"tagName":"ID","tag":"637fa607cd453680560f5ec6"}]){id}}',
+    addOrderToTerminalTicket: [
+      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:115,price:54.00,quantity:1,portion:"Normal",orderTags:""){orders{uid}}}',
+    ],
     addProduct: [
-      'mutation m {\n                addProduct(name: "Kapalı Kıymalı Pide - 1.5 Porsiyon", groupCode: "Entegrasyon", portions: {name: "Normal", price:1}) {\n                  id\n                }\n              }',
-      'mutation m {\n                addProduct(name: "Beyaz Peynirli Çift Yumurtalı Pide (Açık) - 1 Porsiyon", groupCode: "Entegrasyon", portions: {name: "Normal", price:1}) {\n                  id\n                }\n              }',
-      'mutation m {\n                addProduct(name: "Kuşbaşılı Kaşarlı Pide (Açık) - 1.5 Porsiyon", groupCode: "Entegrasyon", portions: {name: "Normal", price:1}) {\n                  id\n                }\n              }',
-      'mutation m {\n                addProduct(name: "Kavurmalı Kaşarlı Pide (Kapalı) - 1 Porsiyon", groupCode: "Entegrasyon", portions: {name: "Normal", price:1}) {\n                  id\n                }\n              }',
+      'mutation m {\n                addProduct(name: "Muzlu Mualla - 425 cc.", groupCode: "Entegrasyon", portions: {name: "Normal", price:1}) {\n                  id\n                }\n              }',
     ],
     getProduct: [
-      { name: 'Kapalı Kıymalı Pide - 1.5 Porsiyon' },
-      { name: 'Beyaz Peynirli Çift Yumurtalı Pide (Açık) - 1 Porsiyon' },
-      { name: 'Kuşbaşılı Kaşarlı Pide (Açık) - 1.5 Porsiyon' },
-      { name: 'Kavurmalı Kaşarlı Pide (Kapalı) - 1 Porsiyon' },
+      {
+        name: 'Muzlu Mualla - 425 cc.',
+      },
     ],
     addOrderToTerminalTicketWithProduct: [
-      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:{productId},price:93.00,quantity:1,portion:"Normal",orderTags:""){orders{uid}}}',
-      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:{productId},price:57.00,quantity:1,portion:"Normal",orderTags:""){orders{uid}}}',
-      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:{productId},price:109.50,quantity:1,portion:"Normal",orderTags:""){orders{uid}}}',
-      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:{productId},price:82.00,quantity:1,portion:"Normal",orderTags:""){orders{uid}}}',
-    ],
-    addOrderToTerminalTicket: [
-      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:5652,price:30.00,quantity:1,portion:"Normal",orderTags:""){orders{uid}}}',
+      'mutation m {addOrderToTerminalTicket(terminalId:"{terminalId}",productId:{productId},price:54.00,quantity:1,portion:"Normal",orderTags:""){orders{uid}}}',
     ],
     addCalculationToTerminalTicket:
       'mutation m { addCalculationToTerminalTicket(terminalId:"{terminalId}",calculationName:"Joker İskonto",amount:0.00){id}}',
     changeEntityOfTerminalTicket:
-      'mutation m {changeEntityOfTerminalTicket (terminalId:"{terminalId}",type:"Müşteriler",name:"Gizem h - 542 772 53 19"){uid}}',
+      'mutation m {changeEntityOfTerminalTicket (terminalId:"{terminalId}",type:"Müşteriler",name:"Furkan N - 5302790647"){uid}}',
     closeTerminalTicket:
       'mutation m {closeTerminalTicket(terminalId:"{terminalId}")}',
     getTerminalTickets:
@@ -49,12 +41,6 @@ let queries = {
       'mutation m {\n                        postTicketRefreshMessage(id: 0) {\n                          id\n                        }\n                      }',
     postBroadcastMessage:
       'mutation m { postBroadcastMessage(message: "ENT-YeniSiparis-ys") {\n                              message\n                            }\n                          }',
-  },
-  order: {
-    id: '1554483686',
-    pid: '1554483686',
-    slug: 'ys',
-    restaurantId: '63617059d55d1a6201061679',
   },
 }
 const Query = require('./bin/lib/query')
