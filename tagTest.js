149ace52fcb6f793d7f68ed8755809392bd565ee
marian <golgisistem@gmail.com>
2022-12-10 21:45:09 +0200

    test

--- tagTest.js
+++ tagTest.js
@@ -2,14 +2,14 @@ const Query = require('./bin/lib/query')
 
 async function test() {
   let checkTag = `{
-        getTickets(start: "2022-12-06 00:00", end: "2022-12-10 21:30", isClosed: false) {
-          id
-          tags {
-            tagName
-            tag
-          }
-        }
-      }`
+    getTickets(start: "2022-12-10 22:20", end: "2022-12-10 22:45", isClosed: false) {
+      id
+      tags {
+        tagName
+        tag
+      }
+    }
+  }`
   const q = new Query()
   let tags = await q.getQueryWithText(checkTag)
   console.log(tags)
