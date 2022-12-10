82aa4806ee32e8d870379f20c066ff268b8b6dae
marian <golgisistem@gmail.com>
2022-12-10 22:00:15 +0200

    test

--- tagTest.js
+++ tagTest.js
@@ -1,4 +1,6 @@
 const Query = require('./bin/lib/query')
+const _ = require('lodash')
+const { asyncFilter } = require('./bin/lib/helpers')
 
 async function test() {
   let checkTag = `{
@@ -12,6 +14,25 @@ async function test() {
   }`
   const q = new Query()
   let tags = await q.getQueryWithText(checkTag)
+  if (tags) {
+    if (_.has(tags, 'getTickets')) {
+      tags = tags['getTickets']
+      let filtered = await asyncFilter(tags, async (v) => {
+        let result = { id: v['id'], filter: [] }
+        if (Array.isArray(v['tags'])) {
+          let filter = await asyncFilter(v['tags'], async (t) => {
+            return t['tagName'] == 'ID'
+          })
+
+          result['filter'] = filter
+        }
+
+        return result
+      })
+
+      console.log(filtered)
+    }
+  }
   console.log(tags)
 }
 
