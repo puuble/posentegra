fb7066ed531f36338f294c07204aeab1adbb7fde
marian <golgisistem@gmail.com>
2023-01-12 19:18:42 +0200

    test

---
+++ bin/setup.js
@@ -0,0 +1,19 @@
+const fs = require('fs')
+const Sambapos = require('./lib/sambapos')
+const sambapos = new Sambapos()
+async function main() {
+  let s = await sambapos.authCheck()
+  console.log(s)
+  let json = `{
+        "pos": {
+            "host": "127.0.0.1",
+            "port": "9000",
+            "name": "test",
+            "username": "Entegrasyon",
+            "password": "73737373",
+            "client_id": "Entegrasyon"
+        }
+    }`
+  //fs.writeFileSync('./tmp/enviroment.json', json)
+}
+main()
