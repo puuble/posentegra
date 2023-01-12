b057e8dc7608de28ed9044e931c396745473667f
marian <golgisistem@gmail.com>
2023-01-12 21:09:00 +0200

    test

--- updateV2.bat
+++ updateV2.bat
@@ -1,3 +1,4 @@
 cd "C:/"
 cd peClientv2
-call git fetch origin v2 && git reset --hard FETCH_HEAD && git clean -df && npm install && node ./bin/update.js && pm2 stop all && pm2 restart all
+call pm2 stop all
+call git fetch origin v2 && git reset --hard FETCH_HEAD && git clean -df && npm install && node ./bin/update.js && pm2 restart all
