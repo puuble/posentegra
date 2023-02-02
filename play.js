a5f4c676a6e041b386154fbe6fc56bf5f4d38ad0
marian <golgisistem@gmail.com>
2023-02-02 21:25:22 +0200

    test

--- play.js
+++ play.js
@@ -6,7 +6,7 @@ function playSound() {
   // const sound = spawn('C:\\Windows\\System32\\cmd.exe', ['/c', 'start', ' Media.SoundPlayer', '/path/to/sound.wav'])
   const sound = spawn('powershell.exe', [
     '-Command',
-    '(New-Object Media.SoundPlayer "C:\\peClientV2\\OnayBekliyor.wav").Play(); Start-Sleep -s 3; Exit;',
+    '(New-Object Media.SoundPlayer "./OnayBekliyor.wav").Play(); Start-Sleep -s 3; Exit;',
   ])
 
   sound.on('close', (code) => {
