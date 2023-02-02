4e999febb2bc6db501641d7d1e83101d10fb0fe3
marian <golgisistem@gmail.com>
2023-02-02 21:26:35 +0200

    test

--- play.js
+++ play.js
@@ -2,11 +2,11 @@ const { spawn } = require('child_process')
 let p = './OnayBekliyor.wav'
 function playSound() {
   //const sound = spawn('afplay', ['-v', '100', p])
-  //const sound = spawn('start', ['mplayer', p])
+
   // const sound = spawn('C:\\Windows\\System32\\cmd.exe', ['/c', 'start', ' Media.SoundPlayer', '/path/to/sound.wav'])
   const sound = spawn('powershell.exe', [
     '-Command',
-    '(New-Object Media.SoundPlayer "./OnayBekliyor.wav").Play(); Start-Sleep -s 3; Exit;',
+    '(New-Object Media.SoundPlayer "' + p + '").Play(); Start-Sleep -s 3; Exit;',
   ])
 
   sound.on('close', (code) => {
