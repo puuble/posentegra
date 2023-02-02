b6855fac7a6c02a719974452b81297b168eaa59e
marian <golgisistem@gmail.com>
2023-02-02 21:10:56 +0200

    test

--- play.js
+++ play.js
@@ -2,7 +2,8 @@ const { spawn } = require('child_process')
 let p = './OnayBekliyor.wav'
 function playSound() {
   //const sound = spawn('afplay', ['-v', '100', p])
-  const sound = spawn('start', ['mplayer', p])
+  //const sound = spawn('start', ['mplayer', p])
+  const sound = spawn('C:\\Windows\\System32\\cmd.exe', ['/c', 'start', 'mplayer', '/path/to/sound.wav'])
   sound.on('close', (code) => {
     if (code === 0) {
       // ws.send('played')
