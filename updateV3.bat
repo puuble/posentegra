74023acc11ff88ddd5bb5680ddf869e97c01058a
marian <golgisistem@gmail.com>
2023-02-26 22:26:19 +0200

    test

--- updateV3.bat
+++ updateV3.bat
@@ -1,4 +1,4 @@
 cd "C:/"
 cd peClientV3
 call pm2 stop all
-call git fetch origin v3 && git reset --hard FETCH_HEAD && git clean -df && npm install && node ./bin/update.js && pm2 restart all
+call git fetch origin v3 && git reset --hard FETCH_HEAD && git clean -df && npm install  && pm2 restart all
