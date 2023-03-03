736dbe7b084ed64785d8fbfb901d7df19cf73e35
marian <golgisistem@gmail.com>
2023-03-03 12:34:38 +0200

    test

--- bin/lib/socket.js
+++ bin/lib/socket.js
@@ -93,47 +93,47 @@ class Socket {
   }
   async order(data) {
     try {
+      let pos_ticket = null
       this.env = await getEnvironment()
-      if (!_.has(data, 'message')) {
-        console.log(data, 'datageldi')
-      } else {
+      if (_.has(data, 'message')) {
         let message = JSON.parse(data['message'])
-
         const q = new Query(message)
-        let pos_ticket = await q.init()
-
-        let slug = message['order']['slug']
-        let restaurantId = message['order']['restaurantId']
-
-        let result = {
-          message: {
-            pos_ticket,
-            orderId: message['order']['pid'],
-          },
-          channel: data['channel'],
-          sender: data['user']['id'],
-          receiver: data['receiver'],
-          broadcast: false,
-        }
-        if (_.has(this.env.restaurants, restaurantId)) {
-          if (slug == 'ty') {
-            const TY = require('./ty')
-            let tyData = this.env.restaurants[restaurantId]
-            console.log(tyData, 'TY ONAYLAMA')
-            const ty = new TY(tyData['ty'])
-            await ty.set500(message['order']['pid'])
-          }
-        }
-
-        let last = {
-          receive: result,
-        }
-        if (pos_ticket) {
-          last.send = await this.api.send(result)
-        }
-
-        return last
+        pos_ticket = await q.init()
+      } else {
+        pos_ticket = data['post_ticket']
       }
+
+      let slug = message['order']['slug']
+      let restaurantId = message['order']['restaurantId']
+
+      let result = {
+        message: {
+          pos_ticket,
+          orderId: message['order']['pid'],
+        },
+        channel: data['channel'],
+        sender: data['user']['id'],
+        receiver: data['receiver'],
+        broadcast: false,
+      }
+      if (_.has(this.env.restaurants, restaurantId)) {
+        if (slug == 'ty') {
+          const TY = require('./ty')
+          let tyData = this.env.restaurants[restaurantId]
+          console.log(tyData, 'TY ONAYLAMA')
+          const ty = new TY(tyData['ty'])
+          await ty.set500(message['order']['pid'])
+        }
+      }
+
+      let last = {
+        receive: result,
+      }
+      if (pos_ticket) {
+        last.send = await this.api.send(result)
+      }
+
+      return last
     } catch (err) {
       console.error(err)
       return null
