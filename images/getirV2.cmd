7abe7ff71d291f6baab870570f5f2ed394f05398
marian <puubles@gmail.com>
2023-10-11 20:11:16 +0300

    tetst

---
+++ images/getirV2.cmd
@@ -0,0 +1,8 @@
+@powershell -Command (New-Object Media.SoundPlayer "C:\peClientV3\images\getir.wav").Play(); Start-Sleep -s 3; Exit;
+timeout 3
+@powershell -Command (New-Object Media.SoundPlayer "C:\peClientV3\images\getir.wav").Play(); Start-Sleep -s 3; Exit;
+timeout 2
+@powershell -Command (New-Object Media.SoundPlayer "C:\peClientV3\images\getir.wav").Play(); Start-Sleep -s 3; Exit;
+timeout 1
+@powershell -Command (New-Object Media.SoundPlayer "C:\peClientV3\images\getir.wav").Play(); Start-Sleep -s 3; Exit;
+exit
