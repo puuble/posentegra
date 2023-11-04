7f827f817361ce30f251503ea5e280aff3abe489
marian <puubles@gmail.com>
2023-11-04 10:45:00 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -1,150 +1,151 @@
-const Query = require('../bin/lib/query')
-var express = require('express')
-const path = require('path')
-const Api = require('../bin/lib/api')
-const DB = require('../bin/lib/database')
-const { getEnv, _asyncrequest, m_exec } = require('../bin/lib/helpers')
-var router = express.Router()
-const db = new DB()
+const Query = require("../bin/lib/query");
+var express = require("express");
+const path = require("path");
+const Api = require("../bin/lib/api");
+const DB = require("../bin/lib/database");
+const { getEnv, _asyncrequest, m_exec } = require("../bin/lib/helpers");
+var router = express.Router();
+const db = new DB();
 
-router.get('/upload-panel', async function (req, res) {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'))
-})
-router.get('/', async function (req, res) {
-  res.sendFile(path.join(__dirname, '../dist', 'index.html'))
-})
-router.get('/sendAgain', async function (req, res) {
-  let env = await getEnv()
-  let sonuc = false
+router.get("/upload-panel", async function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
+
+router.get("/", async function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
+
+router.get("/onayKodu", async function (req, res) {
+  res.sendFile(path.join(__dirname, "../dist", "index.html"));
+});
+
+router.get("/sendAgain", async function (req, res) {
+  let env = await getEnv();
+  let sonuc = false;
   if (req.query.id) {
     sonuc = await _asyncrequest(
-      '/api/again?id=' + req.query.id,
-      'GET',
+      "/api/again?id=" + req.query.id,
+      "GET",
       {},
       {
-        Authorization: 'Bearer ' + env.token,
+        Authorization: "Bearer " + env.token,
       }
-    ).catch((e) => console.log('SERVERDAN', e))
+    ).catch((e) => console.log("SERVERDAN", e));
   }
 
-  return res.json(sonuc)
-})
+  return res.json(sonuc);
+});
 
-router.get('/odemeTipiGonder', async (req, res, next) => {
-  let env = await getEnv()
+router.get("/odemeTipiGonder", async (req, res, next) => {
+  let env = await getEnv();
   let result = {
-    channel: 'getOdemeTipi',
+    channel: "getOdemeTipi",
     message: { ot: req.query.ot },
     sender: env.userId,
     receiver: env.userId,
-  }
-  let sonuc = await _asyncrequest('/api/trigger', 'POST', result, {
-    Authorization: 'Bearer ' + env.token,
-  }).catch((e) => console.log('SERVERDAN'))
-  console.log(sonuc, 'SERVERDAN')
+  };
+  let sonuc = await _asyncrequest("/api/trigger", "POST", result, {
+    Authorization: "Bearer " + env.token,
+  }).catch((e) => console.log("SERVERDAN"));
+  console.log(sonuc, "SERVERDAN");
 
-  return res.json({ success: true })
-})
+  return res.json({ success: true });
+});
 
-router.get('/tarihGonder', async (req, res, next) => {
-  let id = req.query.id
+router.get("/tarihGonder", async (req, res, next) => {
+  let id = req.query.id;
 
   if (id) {
-    const env = await getEnv()
-    let api = new Api()
-    let query = new Query()
+    const env = await getEnv();
+    let api = new Api();
+    let query = new Query();
 
     let frm = {
       message: {
         id,
       },
-      channel: 'tarihGonder',
+      channel: "tarihGonder",
       sender: env.userId,
       receiver: env.userId,
       broadcast: false,
-    }
+    };
 
-    let send = await api.send(frm)
+    let send = await api.send(frm);
     let q = `mutation m1 {
           postBroadcastMessage(message: "ENT- Wp Gönder - ${send.result.samba_id}") {
             message
           }
-        }`
-    await query.getQueryWithText(q)
-    return res.json({ success: true })
+        }`;
+    await query.getQueryWithText(q);
+    return res.json({ success: true });
   }
 
-  res.json({ success: true })
-})
+  res.json({ success: true });
+});
 
-router.get('/raporGonder', async (req, res, next) => {
+router.get("/raporGonder", async (req, res, next) => {
   try {
-    const env = await getEnv()
-    let api = new Api()
-    console.log(req.query.name)
+    const env = await getEnv();
+    let api = new Api();
+    console.log(req.query.name);
     let frm = {
       message: {
         name: req.query.name,
       },
-      channel: 'raporGonder',
+      channel: "raporGonder",
       sender: env.userId,
       receiver: env.userId,
       broadcast: true,
-    }
-    console.log(frm)
-    await api.send(frm)
-    return res.json({ success: true })
+    };
+    console.log(frm);
+    await api.send(frm);
+    return res.json({ success: true });
   } catch (error) {
-    console.log(error)
+    console.log(error);
   }
-})
+});
 
-router.post('/upload', async (req, res) => {
-  let env = await getEnv()
-  let trigger = await _asyncrequest(
-    '/api/authenticateWithToken',
-    'POST',
-    { token: env.token },
-    {}
-  ).catch((e) => {
-    req.session.fail = true
+router.post("/upload", async (req, res) => {
+  let env = await getEnv();
+  let trigger = await _asyncrequest("/api/authenticateWithToken", "POST", { token: env.token }, {}).catch((e) => {
+    req.session.fail = true;
     //console.log(e)
     // return res.redirect(301, '/')
-  })
+  });
 
   if (!trigger) {
-    req.session.fail = true
+    req.session.fail = true;
   }
 
   if (trigger.success) {
-    let api = new Api()
-    let filename = ''
-    const bb = busboy({ headers: req.headers })
-    bb.on('file', async (name, file, info) => {
-      filename = info.filename
-      const saveTo = path.join(__dirname, filename)
-      file.pipe(fs.createWriteStream(saveTo))
-      console.log(filename)
+    let api = new Api();
+    let filename = "";
+    const bb = busboy({ headers: req.headers });
+    bb.on("file", async (name, file, info) => {
+      filename = info.filename;
+      const saveTo = path.join(__dirname, filename);
+      file.pipe(fs.createWriteStream(saveTo));
+      console.log(filename);
       await api.sendFile(
         {
           file: {
             value: req.file,
             options: {
               filename: filename,
-              contentType: 'text/plain',
+              contentType: "text/plain",
             },
           },
         },
         {}
-      )
-    })
+      );
+    });
 
-    req.pipe(bb)
+    req.pipe(bb);
   } else {
-    req.session.fail = true
+    req.session.fail = true;
   }
 
-  return res.redirect(301, '/')
-})
+  return res.redirect(301, "/");
+});
 
-module.exports = router
+module.exports = router;
--- test.js
+++ test.js
@@ -11,3 +11,28 @@ player.play("./images/getir.mp3", (error) => {
     console.error("Error playing sound:", error);
   }
 });
+const Speaker = require("speaker");
+const fs = require("fs");
+
+// Create a readable stream from your MP3 sound file
+const soundFile = fs.createReadStream("./images/getir.mp3");
+
+// Create a speaker instance to play the audio
+const speaker = new Speaker({
+  channels: 2, // Stereo (change as needed)
+  bitDepth: 16, // 16-bit (change as needed)
+  sampleRate: 44100, // 44.1kHz (change as needed)
+});
+
+// Pipe the MP3 sound file to the speaker
+soundFile.pipe(speaker);
+
+// Handle any errors
+speaker.on("error", (err) => {
+  console.error("Error playing sound:", err);
+});
+
+// Listen for when the sound has finished playing
+speaker.on("close", () => {
+  console.log("Sound finished playing");
+});
