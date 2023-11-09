5c2ee1ed1aab86e33dbf5213227c18ba862c1cbf
marian <puubles@gmail.com>
2023-11-09 21:15:07 +0200

    test

--- routes/index.js
+++ routes/index.js
@@ -115,7 +115,7 @@ router.get("/push", async (req, res, next) => {
           title: req.query.title,
           body: req.query.body,
           icon: "https://posentegra.com/images/logo256.png",
-          deep_link: "https://bossentegra.com/dashboard",
+          deep_link: "https://bossentegra.com/notifications",
           hide_notification_if_site_has_focus: true,
         },
         data: {
